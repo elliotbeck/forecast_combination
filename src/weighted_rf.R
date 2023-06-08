8cecbc89dac3e78487109ff64438bf2fba5dadfe
Elliot Beck <elliot.beck@hotmail.com>
2023-06-08 11:02:12 +0200

    Implement ranger rf function

--- src/weighted_rf.R
+++ src/weighted_rf.R
@@ -1,5 +1,5 @@
 # Libraries
-library(randomForest)
+library(ranger)
 library(pmlbr)
 library(CVXR)
 library(reshape)
@@ -11,8 +11,8 @@ source("src/qis.R")
 data <- fetch_data("225_puma8NH")
 
 # Set simulation parameters
-set.seed(1)
-n_obs <- list(1000, 2000, 3000, 4000, 5000)
+set.seed(66)
+n_obs <- list(200, 400, 600, 800, 1000, 2000, 3000, 4000, 5000)
 n_sim <- 2
 
 # Set up dataframe to store results
@@ -40,80 +40,80 @@ for (i in n_obs) {
         lm_model <- lm(target ~ ., data = train_data)
         lm_predictions <- predict(lm_model, test_data)
 
-        # Random forest benchmark
-        rf_model <- randomForest(target ~ .,
+        # Random forest benchmark with ranger
+        rf_model <- ranger(
+            target ~ .,
             data = train_data,
-            nodesize = 1,
+            min.node.size = 1
         )
 
-        rf_predictions <- as.data.frame(predict(
+        rf_predictions <- predict(
             rf_model,
             test_data
-        ))
+        )$predictions
 
         #  Weighted random forest
-        # Calculate weights
-        rf_predictions_train <- as.data.frame(predict(
+        rf_predictions_train <- predict(
             rf_model,
             train_data,
             predict.all = TRUE
-        ))
+        )$predictions
         rf_residuals <- rf_predictions_train - train_data$target
 
-        # Calculate covariance matrix and mean vector
+        # Calculate covariance matrix and mean vector of residuals
         mean_vector <- colMeans(rf_residuals)
         cov_matrix <- cov(rf_residuals)
         cov_matrix_shrinkage <- qis(rf_residuals)
 
-        # Calculate weights
+        # Calculate weights without shrinkage
         w <- Variable(ncol(rf_residuals))
         objective <- quad_form(w, cov_matrix)
-        # objective <- norm2((t(w) %*% mean_vector)) + quad_form(w, cov_matrix_shrinkage)
+        # objective <- norm2((t(w) %*% mean_vector)) + quad_form(w, cov_matrix)
         constraints <- list(sum(w) == 1)
         prob <- Problem(Minimize(objective), constraints)
         solution <- solve(prob)
-        hist(solution$getValue(w))
 
         #  Weighted random forest predictions
-        rf_predictions_weighted <- as.data.frame(predict(
+        rf_predictions_weighted <- predict(
             rf_model,
             test_data,
             predict.all = TRUE
-        ))
+        )$predictions
 
         #  Multiply predictions by weights per row
         rf_predictions_weighted <- as.matrix(rf_predictions_weighted) %*%
             as.numeric(solution$getValue(w))
 
-        # Calculate weights
+        # Calculate weights with shrinkage
         w <- Variable(ncol(rf_residuals))
         objective <- quad_form(w, cov_matrix_shrinkage)
         # objective <- norm2((t(w) %*% mean_vector)) + quad_form(w, cov_matrix_shrinkage)
         constraints <- list(sum(w) == 1)
         prob <- Problem(Minimize(objective), constraints)
         solution <- solve(prob)
-        hist(solution$getValue(w))
 
         #  Weighted random forest predictions
-        rf_predictions_weighted_shrinkage <- as.data.frame(predict(
+        rf_predictions_weighted_shrinkage <- predict(
             rf_model,
             test_data,
             predict.all = TRUE
-        ))
+        )$predictions
 
         #  Multiply predictions by weights per row
-        rf_predictions_weighted_shrinkage <- as.matrix(rf_predictions_weighted_shrinkage) %*% as.numeric(solution$getValue(w))
+        rf_predictions_weighted_shrinkage <- as.matrix(rf_predictions_weighted_shrinkage) %*%
+            as.numeric(solution$getValue(w))
 
         #  Store results
         results[k, ] <- c(
             i,
             rmse(mean(train_data$target), test_data$target), # naive benchmark
             rmse(lm_predictions, test_data$target), # linear regression
-            rmse(rf_predictions[, 1], test_data$target), # random forest 1/n weights
+            rmse(rf_predictions, test_data$target), # random forest 1/n weights
             rmse(rf_predictions_weighted, test_data$target), # random forest weighted
             rmse(rf_predictions_weighted_shrinkage, test_data$target) # random forest weighted shrinkage
         )
         k <- k + 1
+        print(k)
     }
 }
 
@@ -127,9 +127,35 @@ results_mean <- aggregate(
 save(results_mean, file = "results/weighted_rf_mean.RData")
 save(results, file = "results/weighted_rf_all.RData")
 
-# Plot results df
+# # Load the results
+# load("results/weighted_rf_mean.RData")
+# load("results/weighted_rf_all.RData")
+
+# # Set rmse_rf as baseline
+# results_mean[, 2:6] <- results_mean[, 2:6] / results_mean$rmse_rf
+
+# Convert results to long format
 results_mean_long <- melt(results_mean, id.vars = "Group.1")
-ggplot(results_mean_long, aes(x = Group.1, y = value, color = variable)) +
+results_long <- melt(results, id.vars = "n_obs")
+
+# Filter out naive benchmark and lm
+results_mean_long <- results_mean_long[results_mean_long$variable != "rmse_naive", ]
+results_mean_long <- results_mean_long[results_mean_long$variable != "rmse_lm", ]
+results_long <- results_long[results_long$variable != "rmse_naive", ]
+results_long <- results_long[results_long$variable != "rmse_lm", ]
+
+# # Plot all results
+# ggplot(results_long, aes(x = as.factor(n_obs), y = value, color = variable)) +
+#     geom_boxplot() +
+#     theme_minimal() +
+#     labs(
+#         x = "Number of observations",
+#         y = "RMSE",
+#         color = "Model"
+#     )
+
+# Plot mean results df
+plot <- ggplot(results_mean_long, aes(x = Group.1, y = value, color = variable)) +
     geom_line() +
     geom_point() +
     theme_minimal() +
@@ -137,5 +163,5 @@ ggplot(results_mean_long, aes(x = Group.1, y = value, color = variable)) +
         x = "Number of observations",
         y = "RMSE",
         color = "Model"
-    ) +
-    scale_x_continuous(breaks = n_obs)
+    )
+ggsave("results/weighted_rf_unbiased.png", plot)
