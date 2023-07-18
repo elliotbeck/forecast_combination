f891713ad009510e5b011b86256833a1cc3df26d
Elliot Beck <elliot.beck@htomail.com>
2023-07-18 07:57:41 +0000

    Increase analysis grid and delete old plots

--- src/analysis/weighted_rf_analysis.R
+++ src/analysis/weighted_rf_analysis.R
@@ -13,47 +13,47 @@ datasets <- read.table("metadata/datasets.txt", header = TRUE)
 
 #  Load results
 load_datasets <- function(dataset) {
-  results <- get(load(paste0("results/weighted_rf_unbiased_long_only_", dataset, ".RData")))
+  results <- get(load(paste0("results/weighted_rf_", dataset, ".RData")))
   results$dataset <- dataset
   return(results)
 }
 results <- lapply(datasets$datasets, load_datasets)
 results <- do.call(rbind, results)
 
-# Calculate RMSE ratios
-results$rmse_rf_weighted_ratio_1 <- results$rmse_rf_weighted_1 / results$rmse_rf
-results$rmse_rf_weighted_ratio_1_6 <- results$rmse_rf_weighted_1_6 / results$rmse_rf
-results$rmse_rf_weighted_ratio_100 <- results$rmse_rf_weighted_100 / results$rmse_rf
-results$rmse_rf_weighted_shrinkage_ratio_1 <- results$rmse_rf_weighted_shrinkage_1 / results$rmse_rf
-results$rmse_rf_weighted_shrinkage_ratio_1_6 <- results$rmse_rf_weighted_shrinkage_1_6 /
-  results$rmse_rf
-results$rmse_rf_weighted_shrinkage_ratio_100 <- results$rmse_rf_weighted_shrinkage_100 /
-  results$rmse_rf
+#  Calculate ratios compared to random forest
+results_ratios_rf <- results
+results_ratios_rf[, 2:(ncol(results_ratios_rf) - 1)] <- results_ratios_rf[
+  , 2:(ncol(results_ratios_rf) - 1)
+] / results_ratios_rf$rmse_rf
 
 #  Convert to long format
-results_long <- melt(
-  results,
+results_ratios_rf_long <- melt(
+  results_ratios_rf,
   measure.vars = c(
-    "rmse_rf_weighted_ratio_1",
-    "rmse_rf_weighted_shrinkage_ratio_1",
-    "rmse_rf_weighted_ratio_1_6",
-    "rmse_rf_weighted_shrinkage_ratio_1_6",
-    "rmse_rf_weighted_ratio_100",
-    "rmse_rf_weighted_shrinkage_ratio_100"
+    "rmse_rf_weighted_1",
+    "rmse_rf_weighted_shrinkage_1",
+    "rmse_rf_weighted_1.5",
+    "rmse_rf_weighted_shrinkage_1.5",
+    "rmse_rf_weighted_2",
+    "rmse_rf_weighted_shrinkage_2",
+    "rmse_rf_weighted_2.5",
+    "rmse_rf_weighted_shrinkage_2.5",
+    "rmse_rf_weighted_100",
+    "rmse_rf_weighted_shrinkage_100"
   ),
   id.vars = c("dataset", "n_obs"),
 )
 
 # Change names of number of observations
-results_long$n_obs <- paste0("n=", results_long$n_obs)
-results_long$n_obs <- factor(
-  results_long$n_obs,
-  levels = paste0("n=", c(200, 400, 600, 800, 1000, 2000, 3000, 4000, 5000))
+results_ratios_rf_long$n_obs <- paste0("n = ", results_ratios_rf_long$n_obs)
+results_ratios_rf_long$n_obs <- factor(
+  results_ratios_rf_long$n_obs,
+  levels = paste0("n = ", c(200, 400, 600, 800, 1000, 2000, 3000, 4000, 5000))
 )
 
 # Plot RMSE of all models per number of observations as boxplots
 plot <- ggplot(
-  results_long,
+  results_ratios_rf_long,
   aes(x = variable, y = value, fill = variable)
 ) +
   geom_boxplot() +
@@ -61,16 +61,17 @@ plot <- ggplot(
   theme(legend.position = "none") +
   labs(x = NULL, y = NULL) +
   theme(axis.text.x = element_blank()) +
-  scale_fill_manual(values = rep(c("#7CAE00", "#00BFc4", 3))) +
+  scale_fill_manual(values = rep(c("#7CAE00", "#00BFc4"), 5)) +
   geom_hline(yintercept = 1, linetype = "dashed", color = "red") +
-  scale_y_continuous(limits = c(0.4, 2)) +
-  scale_x_discrete(labels = c(rep("sample cov", 3), rep("nls", 3))) +
+  ylim(c(0.5, 1.75)) +
   facet_wrap(~n_obs, nrow = 1, strip.position = "bottom") +
   stat_summary(fun.y = mean, geom = "point", shape = 23, size = 2, fill = "red")
 ggsave("results/weighted_rf_rmse_ratios.pdf", plot)
 
 # Only keep 334_mv dataset
-results_long_334_mv <- results_long[results_long$dataset == "344_mv", ]
+results_long_334_mv <- results_ratios_rf_long[
+  results_ratios_rf_long$dataset == "344_mv",
+]
 plot <- ggplot(
   results_long_334_mv,
   aes(x = variable, y = value, fill = variable)
@@ -80,112 +81,54 @@ plot <- ggplot(
   theme(legend.position = "none") +
   labs(x = NULL, y = NULL) +
   theme(axis.text.x = element_blank()) +
-  scale_fill_manual(values = c("#7CAE00", "#00BFc4")) +
+  scale_fill_manual(values = rep(c("#7CAE00", "#00BFc4"), 5)) +
   geom_hline(yintercept = 1, linetype = "dashed", color = "red") +
-  # scale_y_continuous(limits = c(0.4, 2)) +
-  scale_x_discrete(labels = c("sample cov", "nls")) +
   facet_wrap(~n_obs, nrow = 1, strip.position = "bottom") +
   stat_summary(fun.y = mean, geom = "point", shape = 23, size = 2, fill = "red")
 ggsave("results/weighted_rf_rmse_ratios_344_mv.pdf", plot)
 
-#  Plot the weights of the weighted random forest
-# Load data
-data <- fetch_data(datasets$datasets[1])
+# Compare short/long vs. long only
+results_rf_weighted_1 <- results
+results_rf_weighted_1[, 2:(ncol(results_rf_weighted_1) - 1)] <-
+  results_rf_weighted_1[
+    ,
+    2:(ncol(results_rf_weighted_1) - 1)
+  ] / results_rf_weighted_1$rmse_rf_weighted_1
 
-# Shuffle rows
-data <- data[sample(seq_len(nrow(data))), ]
+#  Convert to long format
+results_ratios_rf_long <- melt(
+  results_rf_weighted_1,
+  measure.vars = c(
+    "rmse_rf_weighted_shrinkage_1",
+    "rmse_rf_weighted_1.5",
+    "rmse_rf_weighted_shrinkage_1.5",
+    "rmse_rf_weighted_2",
+    "rmse_rf_weighted_shrinkage_2",
+    "rmse_rf_weighted_2.5",
+    "rmse_rf_weighted_shrinkage_2.5"
+  ),
+  id.vars = c("dataset", "n_obs"),
+)
 
-# Data split
-train_data <- data[1:400, ]
-test_data <- data[(400 + 1):(400 + 1000), ]
+# Change names of number of observations
+results_ratios_rf_long$n_obs <- paste0("n = ", results_ratios_rf_long$n_obs)
+results_ratios_rf_long$n_obs <- factor(
+  results_ratios_rf_long$n_obs,
+  levels = paste0("n = ", c(200, 400, 600, 800, 1000, 2000, 3000, 4000, 5000))
+)
 
-# Linear regression benchmark
-lm_model <- lm(target ~ ., data = train_data)
-lm_predictions <- predict(lm_model, test_data)
-
-# Random forest benchmark with ranger
-rf_model <- ranger(target ~ ., data = train_data)
-rf_predictions <- predict(rf_model, test_data)$predictions
-
-#  Weighted random forest
-rf_predictions_train_all <- predict(
-  rf_model, train_data,
-  predict.all = TRUE
-)$predictions
-rf_residuals_train <- train_data$target - rf_predictions_train_all
-
-# Calculate covariance matrix and mean vector of residuals
-mean_vector <- colMeans(rf_residuals_train)
-cov_matrix <- cov(rf_residuals_train)
-cov_matrix_shrinkage <- qis(rf_residuals_train)
-
-# Calculate weights without shrinkage
-w <- Variable(ncol(rf_residuals_train))
-objective <- norm2((t(w) %*% mean_vector)) + quad_form(w, cov_matrix)
-constraints <- list(sum(w) == 1)
-prob <- Problem(Minimize(objective), constraints)
-solution <- solve(prob)
-weights_sample <- data.frame(weights_sample = solution$getValue(w))
-
-# Calculate weights with shrinkage
-w <- Variable(ncol(rf_residuals_train))
-objective <- norm2((t(w) %*% mean_vector)) + quad_form(w, cov_matrix_shrinkage)
-constraints <- list(sum(w) == 1)
-prob <- Problem(Minimize(objective), constraints)
-solution <- solve(prob)
-weights_shrinkage <- data.frame(weights_shrinkage = solution$getValue(w))
-
-#  Use ggplot to plot the weights with histogram
-weights <- cbind(weights_sample, weights_shrinkage)
-weights <- melt(weights)
-weights$variable <- factor(weights$variable, levels = c("weights_sample", "weights_shrinkage"))
-ggplot(weights, aes(x = value, fill = variable)) +
-  geom_histogram(color = "black") +
+# Plot RMSE of all models per number of observations as boxplots
+plot <- ggplot(
+  results_ratios_rf_long,
+  aes(x = variable, y = value, fill = variable)
+) +
+  geom_boxplot() +
   theme_minimal() +
-  scale_fill_manual(values = c("#7CAE00", "#00BFc4")) +
   theme(legend.position = "none") +
   labs(x = NULL, y = NULL) +
-  theme(
-    strip.background = element_blank(),
-    strip.text.x = element_blank()
-  ) +
-  facet_wrap(~variable, scales = "fixed")
-ggsave("results/weighted_rf_weights_long_only_.pdf")
-
-# Compare short/long vs. only long
-# Load results long only
-load_datasets_long <- function(dataset) {
-  results <- get(load(paste0("results/weighted_rf_unbiased_long_only_", dataset, ".RData")))
-  results$dataset <- dataset
-  return(results)
-}
-results_long <- lapply(datasets$datasets, load_datasets_long)
-results_long <- do.call(rbind, results_long)
-# Load results short
-load_datasets_short <- function(dataset) {
-  results <- get(load(paste0("results/weighted_rf_unbiased_", dataset, ".RData")))
-  results$dataset <- dataset
-  return(results)
-}
-results_short <- lapply(datasets$datasets, load_datasets_short)
-results_short <- do.call(rbind, results_short)
-
-# Calculate mean RMSEs
-results_long_mean <- aggregate(
-  results_long[, c(
-    "rmse_naive", "rmse_lm", "rmse_rf",
-    "rmse_rf_weighted", "rmse_rf_weighted_shrinkage"
-  )],
-  by = list(results_long$n_obs),
-  FUN = mean
-)
-results_long_mean$method <- "long"
-results_short_mean <- aggregate(
-  results_short[, c(
-    "rmse_naive", "rmse_lm", "rmse_rf",
-    "rmse_rf_weighted", "rmse_rf_weighted_shrinkage"
-  )],
-  by = list(results_short$n_obs),
-  FUN = mean
-)
-results_short_mean$method <- "short"
+  theme(axis.text.x = element_blank()) +
+  scale_fill_manual(values = c("#00BFc4", rep(c("#7CAE00", "#00BFc4"), 3))) +
+  geom_hline(yintercept = 1, linetype = "dashed", color = "red") +
+  facet_wrap(~n_obs, nrow = 1, strip.position = "bottom") +
+  stat_summary(fun.y = mean, geom = "point", shape = 23, size = 2, fill = "red")
+ggsave("results/weighted_rf_rmse_ratios_1.pdf", plot)
