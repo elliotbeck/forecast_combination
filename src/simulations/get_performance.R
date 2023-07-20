39ef1e59e0eb6282d1dc53f8567b3f0c27e87ccd
Elliot Beck <elliot.beck@htomail.com>
2023-07-20 14:10:38 +0000

    Reorganize performance eval

--- src/simulations/get_performance.R
+++ src/simulations/get_performance.R
@@ -1,31 +1,24 @@
 # Libraries
-library(CVXR)
 source("src/utils/rmse.R")
+source("src/simulations/get_weights.R")
 
 get_performance <- function(
     mean_vector,
     cov_matrix,
-    predictions_train,
     predictions_test,
-    labels_train,
     labels_test,
-    kappa) {
-  w <- Variable(ncol(cov_matrix))
-  objective <- square((t(w) %*% mean_vector)) + quad_form(w, cov_matrix)
-  constraints <- list(
-    sum(w) == 1,
-    sum(abs(w)) <= kappa
-  )
-  prob <- Problem(Minimize(objective), constraints)
-  solution <- solve(prob, num_iter = 100000)
+    kappa,
+    mean,
+    sd) {
+  # Get weights
+  w <- get_weights(mean_vector, cov_matrix, kappa)
 
   # Weighted prediction
-  predictions_weighted_train <- as.matrix(predictions_train) %*% solution$getValue(w)
-  predictions_weighted_test <- as.matrix(predictions_test) %*% solution$getValue(w)
+  predictions_weighted_test <- as.matrix(predictions_test) %*% w
+  predictions_weighted_test <- (predictions_weighted_test * sd) + mean
 
   # Calculate rmse
-  rmse_train <- rmse(predictions_weighted_train, labels_train)
   rmse_test <- rmse(predictions_weighted_test, labels_test)
 
-  return(c(rmse_train, rmse_test))
+  return(rmse_test)
 }
