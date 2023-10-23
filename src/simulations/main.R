14528d1251a67ed729efa721808b4478c0775272
Elliot Beck <elliot.beck@htomail.com>
2023-10-23 15:45:58 +0000

    Change datasets used for anaylsis

--- src/simulations/main.R
+++ src/simulations/main.R
@@ -1,20 +1,20 @@
 # Libraries
-library(pmlbr)
+library(OpenML)
 library(parallel)
 source("src/simulations/get_simulation_dataset.R")
+setOMLConfig(apikey = "c1994bdb7ecb3c6f3c8f3b35f4b47f1f")
 
 # Load names of datasets
-datasets <- summary_stats[summary_stats$task == "regression", ]
-datasets <- datasets[
-  datasets$n_instances >= 6000 & datasets$n_instances <= 100000,
-]
-datasets <- datasets$dataset
-datasets <- datasets[datasets != "294_satellite_image"] # Not a regression task
-datasets <- datasets[datasets != "503_wind"] # Time series, not iid
+datasets <- read.csv("metadata/numerical_regression.csv")
+datasets <- datasets[datasets$Remove == 0, ]
+datasets <- datasets[datasets$checks_passed == 1, ]
+datasets <- datasets[!is.na(datasets$Remove), ]
+datasets <- datasets$dataset_name
 
 # Set simulation parameters
 set.seed(42)
-num_trees <- 500 # Ranger default
+n_cores <- length(datasets)
+n_trees <- 500 # Ranger default
 n_obs <- list(200, 400, 600, 800, 1000, 2000, 3000, 4000, 5000)
 n_sim <- 100
 kappas <- list(1, 1.5, 2, 2.5, 100)
@@ -23,8 +23,8 @@ kappas <- list(1, 1.5, 2, 2.5, 100)
 mclapply(
   datasets,
   get_simulation_dataset_iid,
-  mc.cores = length(datasets),
-  num_trees = num_trees,
+  mc.cores = n_cores,
+  num_trees = n_trees,
   n_obs = n_obs,
   n_sim = n_sim,
   kappas = kappas
